f42919ac274e67ea8cc8cb8d0b5543e07989387b
Sean Smith <SeanSith@users.noreply.github.com>
2025-10-30 18:09:52 -0400

    Docker Desktop added completions we can access

--- config/zsh/shell-completion.zsh
+++ config/zsh/shell-completion.zsh
@@ -1,3 +1,7 @@
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=(/$HOME/.docker/completions $fpath)
+# End of Docker CLI completions
+
 # Load and initialise completion system
 autoload -Uz compinit
 compinit -d "${XDG_DATA_HOME:-$HOME}/.local/share/zsh/.zcompdump"
